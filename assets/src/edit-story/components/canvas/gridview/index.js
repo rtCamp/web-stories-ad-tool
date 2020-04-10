937bec9847b50d1269d7fdebf13a26e097e8b3ba
Wassim Gharbi <wassgha@gmail.com>
2020-04-10 12:05:34 -0700

    Fixed scrollbar on gridview

--- assets/src/edit-story/components/canvas/gridview/index.js
+++ assets/src/edit-story/components/canvas/gridview/index.js
@@ -55,7 +55,7 @@ const Container = styled.div`
 
 const Wrapper = styled(Reorderable)`
   position: relative;
-  overflow-y: scroll;
+  overflow-y: auto;
   overflow-x: hidden;
   flex-grow: 1;
   display: flex;
