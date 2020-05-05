939a567eb1e782d6001f5cc6191938ba0f935bb0
Clinton Volzke <cvolzke@google.com>
2020-05-05 16:19:34 +1000

    webpack_test

---
+++ tests/e2e/app/index.js
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import App from '../../../assets/src/edit-story/app';
+import { createStory } from './_utils';
+
+// @todo: Find better way to mock these.
+const config = {
+  allowedMimeTypes: {
+    image: ['image/png', 'image/jpeg', 'image/jpg', 'image/gif'],
+    audio: [],
+    video: ['video/mp4'],
+  },
+  allowedFileTypes: ['png', 'jpeg', 'jpg', 'gif', 'mp4'],
+  storyId: 1234,
+  api: {
+    stories: (storyId) => Promise.resolve(createStory({ id: storyId })),
+    media: '',
+    fonts: '',
+    link: '',
+    users: '',
+    statuses: '',
+  },
+  metadata: {
+    publisher: {
+      name: '',
+      logo: '',
+    },
+    poster: '',
+  },
+};
+
+const wrapper = document.getElementById('container');
+wrapper ? ReactDOM.render(<App config={config} />, wrapper) : false;
