2464e99bdfaea590af3c64e6f92976f5b964dadf
RahiDroid <rahi.prajapati@rtcamp.com>
2020-11-04 18:21:41 +0530

    Feat: Remove server-side-render in editor and handle stories manually

--- assets/src/latest-stories-block/block/edit.js
+++ assets/src/latest-stories-block/block/edit.js
@@ -18,21 +18,26 @@
  * External dependencies
  */
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 /**
  * WordPress dependencies
  */
-import ServerSideRender from '@wordpress/server-side-render';
+import { useEffect, useState, useRef } from '@wordpress/element';
+import apiFetch from '@wordpress/api-fetch';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
+import StoryPlayer from './storyPlayer';
 import LatestStoriesControls from './latestStoriesControls';
 
 import './edit.css';
 
 const LatestStoriesEdit = ({ attributes, setAttributes }) => {
   const {
+    align,
     viewType,
     numOfStories,
     numOfColumns,
@@ -48,12 +53,90 @@ const LatestStoriesEdit = ({ attributes, setAttributes }) => {
     listViewImageAlignment,
   } = attributes;
 
+  const [fetchedStories, setFetchedStories] = useState([]);
+
+  useEffect(() => {
+    let order,
+      orderBy = '';
+
+    switch (orderByValue) {
+      case 'old-to-new':
+        order = 'asc';
+        break;
+      case 'alphabetical':
+        orderBy = 'title';
+        order = 'asc';
+        break;
+      case 'reverse-alphabetical':
+        orderBy = 'title';
+        order = 'desc';
+        break;
+      default:
+        orderBy = 'date';
+        order = 'desc';
+    }
+
+    const latestPostsQuery = {
+      author: authors,
+      order,
+      orderby: orderBy,
+      per_page: numOfStories,
+    };
+
+    apiFetch({
+      path: `/web-stories/v1/web-story?${new URLSearchParams(
+        latestPostsQuery
+      )}`,
+    })
+      .then((stories) => {
+        setFetchedStories(stories);
+      })
+      .catch((err) => {
+        // Temporarily disabled, show a UI message.
+        // eslint-disable-next-line no-console
+        console.log(err);
+      });
+  }, [authors, numOfStories, orderByValue]);
+
+  const refs = useRef([]);
+  const willShowStoryPoster =
+    'list' === viewType || 'circles' === viewType ? true : isShowingStoryPoster;
+  const willShowTitle = 'circles' === viewType ? false : isShowingTitle;
+  const willShowDate = 'circles' === viewType ? false : isShowingDate;
+  const willShowAuthor = 'circles' === viewType ? false : isShowingAuthor;
+  const viewAllLabel = viewAllLinkLabel
+    ? viewAllLinkLabel
+    : __('View All Stories', 'web-stories');
+
+  useEffect(() => {
+    if (willShowStoryPoster) {
+      return;
+    }
+
+    refs.current = refs.current.slice(0, fetchedStories.length);
+
+    refs.current.forEach((el, index) => {
+      if (global.AmpStoryPlayer) {
+        refs.current[index] = new global.AmpStoryPlayer(
+          global,
+          refs.current[index]
+        );
+        refs.current[index].load();
+      }
+    });
+  }, [fetchedStories, willShowStoryPoster]);
+
+  const blockClasses = classNames(
+    'wp-block-web-stories-latest-stories latest-stories',
+    { [`is-view-type-${viewType}`]: viewType },
+    { [`align${align}`]: align }
+  );
+  const blockStyles = {
+    gridTemplateColumns: `repeat(${numOfColumns}, 1fr)`,
+  };
+
   return (
     <>
-      <ServerSideRender
-        block="web-stories/latest-stories"
-        attributes={attributes}
-      />
       <LatestStoriesControls
         viewType={viewType}
         numOfStories={numOfStories}
@@ -70,12 +153,41 @@ const LatestStoriesEdit = ({ attributes, setAttributes }) => {
         listViewImageAlignment={listViewImageAlignment}
         setAttributes={setAttributes}
       />
+      {fetchedStories && 0 < fetchedStories.length && (
+        <div>
+          <div className={blockClasses} style={blockStyles}>
+            {Object.keys(fetchedStories).map((index) => {
+              const storyData = fetchedStories[index];
+
+              return (
+                <StoryPlayer
+                  key={index}
+                  ref={(el) => (refs.current[index] = el)}
+                  url={storyData.link}
+                  title={storyData.title.rendered}
+                  date={storyData.date}
+                  poster={storyData.featured_media_url}
+                  isShowingStoryPoster={willShowStoryPoster}
+                  listViewImageAlignment={listViewImageAlignment}
+                  isShowingAuthor={willShowAuthor}
+                  isShowingDate={willShowDate}
+                  isShowingTitle={willShowTitle}
+                />
+              );
+            })}
+          </div>
+          {isShowingViewAll && (
+            <div className="latest-stories__archive-link">{viewAllLabel}</div>
+          )}
+        </div>
+      )}
     </>
   );
 };
 
 LatestStoriesEdit.propTypes = {
   attributes: PropTypes.shape({
+    align: PropTypes.string,
     viewType: PropTypes.string,
     numOfStories: PropTypes.number,
     numOfColumns: PropTypes.number,
