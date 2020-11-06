83d3f036f61ebc693e80a2c2bff72360f1745c14
Edi Amin <to.ediamin@gmail.com>
2020-11-06 12:56:56 +0600

    Feat: Initialize Selected Stories block

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -263,6 +263,39 @@ const latestStoriesBlock = {
   },
 };
 
+const selectedStoriesBlock = {
+  ...sharedConfig,
+  entry: {
+    'selected-web-stories-block': './assets/src/selected-stories-block/index.js',
+  },
+  plugins: [
+    process.env.BUNDLE_ANALZYER && new BundleAnalyzerPlugin(),
+    new DependencyExtractionWebpackPlugin({
+      injectPolyfill: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: '../css/[name].css',
+    }),
+    new WebpackBar({
+      name: 'Selected Web Stories Block',
+      color: '#357BB5',
+    }),
+  ].filter(Boolean),
+  optimization: {
+    ...sharedConfig.optimization,
+    splitChunks: {
+      cacheGroups: {
+        stories: {
+          name: 'selected-web-stories-block',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  },
+};
+
 const activationNotice = {
   ...sharedConfig,
   entry: {
@@ -290,4 +323,4 @@ const activationNotice = {
   },
 };
 
-module.exports = [storiesEditor, dashboard, storyEmbedBlock, latestStoriesBlock, activationNotice];
+module.exports = [storiesEditor, dashboard, storyEmbedBlock, latestStoriesBlock, selectedStoriesBlock, activationNotice];
