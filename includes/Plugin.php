f2015d89375e2f196ea1250d194b442ac5f73122
Ankit Gade <ankit.gade@rtcamp.com>
2020-12-11 21:16:11 +0530

    TinyMCE button for classic editor

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -170,7 +170,8 @@ class Plugin {
 	 */
 	public function register() {
 		// Plugin compatibility / polyfills.
-		add_action( 'wp', [ $this, 'load_amp_plugin_compat' ] );
+		add_action( 'init', [ $this, 'load_amp_plugin_compat' ] );
+		add_action( 'init', [ $this, 'includes' ] );
 
 		// Settings.
 		$this->settings = new Settings();
@@ -269,6 +270,16 @@ class Plugin {
 	 */
 	public function load_amp_plugin_compat() {
 		require_once WEBSTORIES_PLUGIN_DIR_PATH . 'includes/compat/amp.php';
+	}
+
+	/**
+	 * Include necessary files.
+	 *
+	 * @return void
+	 */
+	public function includes() {
+		require_once WEBSTORIES_PLUGIN_DIR_PATH . 'includes/compat/tinymce.php';
+		require_once WEBSTORIES_PLUGIN_DIR_PATH . 'includes/functions.php';
 	}
 
 	/**
