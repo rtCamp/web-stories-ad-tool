d76f4b8af46d50a09351d070286bf01024e542dd
Ankit Gade <ankit.gade@rtcamp.com>
2021-02-17 14:41:09 +0530

    Customizer: Add sharp corner option

--- includes/Customizer.php
+++ includes/Customizer.php
@@ -295,6 +295,28 @@ class Customizer {
 			);
 		}
 
+		if ( true === $theme_support['sharp-corners'] ) {
+			$wp_customize->add_setting(
+				self::STORY_OPTION . '[sharp_corners]',
+				[
+					'default' => $theme_support['sharp-corners'],
+					'type'    => 'option',
+				]
+			);
+
+			$wp_customize->add_control(
+				self::STORY_OPTION . '[sharp_corners]',
+				[
+					'type'            => 'checkbox',
+					'section'         => self::SECTION_SLUG,
+					'label'           => __( 'Sharp Corners', 'web-stories' ),
+					'active_callback' => function() {
+						return ( $this->is_option_enabled( 'show_stories' ) && ! $this->is_view_type( 'circles' ) );
+					},
+				]
+			);
+		}
+
 		if ( true === $theme_support['stories-archive-link'] ) {
 			$wp_customize->add_setting(
 				self::STORY_OPTION . '[show_stories_archive_link]',
@@ -477,6 +499,7 @@ class Customizer {
 			'stories_archive_label' => $theme_support['stories-archive-label'],
 			'show_story_poster'     => $theme_support['show-story-poster-default'],
 			'number_of_columns'     => $theme_support['grid-columns-default'],
+			'sharp_corners'         => $theme_support['sharp-corners'],
 		];
 
 		$query_arguments = [
