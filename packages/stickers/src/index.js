562adda54114606c2876739de9ffe32f18a3e44d
Divyaraj Masani <divyaraj.masani@rtcamp.com>
2021-04-19 16:34:52 +0530

    Adds stickers for cooking template.

--- packages/stickers/src/index.js
+++ packages/stickers/src/index.js
@@ -24,6 +24,7 @@ import * as fashionStickers from './fashion';
 import * as fitnessStickers from './fitness';
 import * as travelStickers from './travel';
 import * as wellbeingStickers from './wellbeing';
+import * as cookingStickersV2 from './cooking-02';
 
 export default {
   ...beautyStickers,
@@ -34,4 +35,5 @@ export default {
   ...fitnessStickers,
   ...travelStickers,
   ...wellbeingStickers,
+  ...cookingStickersV2,
 };
