5bea59dc47d8a981b959116a9e11cffe502e16eb
Sayed Taqui <sayedwp@gmail.com>
2021-05-27 20:40:55 +0530

    Update readme

--- assets/src/edit-story/components/header/buttons/download.js
+++ assets/src/edit-story/components/header/buttons/download.js
@@ -156,11 +156,9 @@ function Download() {
     zip.file('config.json', JSON.stringify(storyData));
 
     const readMeText = `
-    In order to view or test this ad as a standalone AMP page locally before uploading it to your platform, it needs to be run on any https-enabled server. For example, you may use the "Web Server for Chrome" chrome extension and view the ad by enabling its HTTPS option.
-
-    Uploading the ad to google ad manager:
-    1. Choose "Code Type" as "AMP", copy the entire content of index.html without formatting and paste it inside the "AMP HTML" textbox.
-    2. All assets of the story ad have been downloaded as part of the zip and have a relative path in index.html, upload those assets and change its file path by inserting macros.
+Uploading ad to google ad manager:
+1. Choose "Code Type" as "AMP", copy the entire content of index.html without formatting and paste it inside the "AMP HTML" textbox.
+2. Your local uploaded assets of the story ad have been downloaded as part of the zip and have a relative path in index.html, please upload those assets and change its file path by inserting macros. If you have used external assets, they would have direct external links in html. 
     `;
 
     zip.file('index.html', markup);
